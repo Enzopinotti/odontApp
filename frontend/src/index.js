f9fc572cea5140b3eb07b1de72fccbcb473efbd3
Enzo Pinotti <enzopinottii@gmail.com>
2025-07-28 21:48:06 -0300

    add: integración de React Query Provider para futuras vistas con datos

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,26 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
 import './styles/main.scss';
 
-import AuthProvider from './context/AuthProvider'; 
+import AuthProvider from './context/AuthProvider';
 import ToastProvider from './context/ToastProvider';
 import ModalProvider from './context/ModalProvider';
+import ReactQueryProvider from './context/ReactQueryProvider'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> 
+    <BrowserRouter>
       <AuthProvider>
         <ToastProvider>
           <ModalProvider>
-            <App />
+            <ReactQueryProvider> 
+              <App />
+            </ReactQueryProvider>
           </ModalProvider>
         </ToastProvider>
       </AuthProvider>
